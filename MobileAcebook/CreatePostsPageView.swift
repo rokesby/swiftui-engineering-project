0136492a7fbe40be5243b9b97998c82dc9dd073a
Santosh <santosh.dasari1@gmail.com>
2024-09-03 21:59:14 +0100

    create-post-page

--- MobileAcebook/CreatePostsPageView.swift
+++ MobileAcebook/CreatePostsPageView.swift
@@ -5,4 +5,32 @@
 //  Created by Santosh Dasari on 02/09/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct CreatePostsPageView: View {
+    @State private var postMessage = ""
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            ZStack {
+                
+                VStack (alignment: .center) {
+                    TextField("What's on your mind...", text: $postMessage)
+                        .padding()
+                    //  .frame(maxHeight: 300)
+                        .scrollContentBackground(.hidden)
+                    Spacer()
+                }
+                Button("Submit") {
+                }
+            }
+        }
+    }
+}
+struct CreatePostsPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePostsPageView()
+    }
+}
