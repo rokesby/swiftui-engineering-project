598d23904d4d1299f9b915d292ef602c5f5adffb
Marya <ms930652210@googlemail.com>
2024-09-03 12:08:42 +0100

    added basic signup form

--- MobileAcebook/SignUpPageView.swift
+++ MobileAcebook/SignUpPageView.swift
@@ -5,4 +5,35 @@
 //  Created by Santosh Dasari on 02/09/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct SignUpPageView: View {
+    
+    @State private var username = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmed_password = ""
+    
+    var body: some View {
+        
+        Form{
+            Section{
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+                TextField("Confirm password", text: $confirmed_password)
+            } header: {
+                Text("Sign up for an account")
+            }
+        }
+        .frame(width: 300.0, height: 150.0)
+                        .scrollContentBackground(.hidden)
+    }
+}
+
+struct SignUpPageView_Previews: PreviewProvider {
+        static var previews: some View {
+            SignUpPageView()
+        }
+    }
+
