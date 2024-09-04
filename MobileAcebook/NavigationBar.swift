b622a444ea122ba8bb02d3c58ab7c7a516491e76
John King <john.king@renlearn.co.uk>
2024-09-04 11:17:52 +0100

    added a functioning nav bar, hidden back button on other pages where necessary

---
+++ MobileAcebook/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  MobileAcebook
+//
+//  Created by John King on 04/09/2024.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
