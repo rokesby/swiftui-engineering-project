dfd6997358080d35e5ad74947356688fbd267e89
John King <john.king@renlearn.co.uk>
2024-09-04 11:22:22 +0100

    previous commit didn't actually do what I needed, this commit is correct

--- MobileAcebook/NavigationBar.swift
+++ MobileAcebook/NavigationBar.swift
@@ -1,18 +1,69 @@
-//
-//  NavigationBar.swift
-//  MobileAcebook
-//
-//  Created by John King on 04/09/2024.
-//
-
 import SwiftUI
 
-struct NavigationBar: View {
+struct CustomNavigationBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            
+            NavigationLink(destination: ContentView()) {
+                VStack {
+                    Image(systemName: "house.fill")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                    Text("Home")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: ProfilePageView()) {
+                VStack {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                    Text("My Profile")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: CreatePostsPageView()) {
+                VStack {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                    Text("Create Post")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: WelcomePageView()) {
+                VStack {
+                    Image(systemName: "arrowshape.turn.up.left.fill")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                    Text("Logout")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .background(Color.gray.opacity(0.8))
     }
 }
 
-#Preview {
-    NavigationBar()
+struct CustomNavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomNavigationBar()
+    }
 }
